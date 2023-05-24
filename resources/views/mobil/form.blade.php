924eb430d847b01bc3a9584e5d94eb718cdb0023
dims572 <dimas050702@gmail.com>
2023-05-24 15:16:00 +0700

    laravel

---
+++ resources/views/mobil/form.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="card">
+        <h2>Form Tambah Mobil</h2>
+    <form method="POST" action="/mobil/simpanData">
+        @csrf
+        <div>
+            <label for="">Nama Mobil</label><br>
+            <input type="text" name="nama_mobil" required><br>
+        </div>
+        <div>
+            <label for="">Merk Mobil</label><br>
+            <input type="text" name="merk_mobil" required><br>
+        </div>
+        <div>
+            <label for="">CC</label><br>
+            <input type="text" name="cc" required><br>
+        </div>
+        <button type="submit">Tambah Data</button>
+    </form>
+    </div>
+</body>
+</html>
